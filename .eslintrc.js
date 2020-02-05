334b24103d6b0100f62b8bdb91a4f8a55bf9c814
Douglas Sappet <doug.sappet@meltwater.com>
2020-02-05 08:54:40 -0500

    chore(test): Getting a couple unit tests. Jasmine and eslint playing nicely

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  "extends": ["airbnb-base", "airbnb-base/legacy", "prettier"],
-  "plugins": ["prettier"],
+  "extends": ["airbnb-base", "airbnb-base/legacy", "prettier", "plugin:jasmine/recommended"],
+  "plugins": ["prettier", "jasmine"],
+  "env": {
+    "jasmine": true
+  },
   "rules": {
     "prettier/prettier": ["error"],
     "no-console": "off"
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,58 +1,72 @@
-const { describe, it, expect, beforeEach } = require('jasmine');
-
-import EsiIncludeWebpackPlugin from './index';
-
+const EsiIncludeWebpackPlugin = require('./index');
 
 describe('index', () => {
   it('should pass', () => {
     expect(true).toBe(true);
   });
+
   describe('EsiIncludeWebpackPlugin', () => {
     describe('buildEsiString', () => {
       it('Shall return an object with "searchString" and "replaceString" properties of type string', () => {
         const result = EsiIncludeWebpackPlugin.buildEsiString({ src: 'hello' });
+
         expect(typeof result).toBe('object');
         expect(typeof result.searchString).toBe('string');
         expect(typeof result.replaceString).toBe('string');
       });
     });
-    describe('manipulateCompulationAssets', () => {
+
+    describe('manipulateCompilationAssets', () => {
+      let compilation;
+      let replacers;
       beforeEach(() => {
-        let compilation = {
+        compilation = {
           assets: {
             'test.html': {
               source() {
-                return '';
+                return 'This string has a key MARCO and has a second one MARCO and a third one CHICKEN';
               }
             },
             'file.html': {
               source() {
-                return '';
+                return 'This string does not have a key';
               }
             },
             'template.ejs': {
               source() {
-                return '';
+                return 'This string a key but it is marco lowercase';
               }
             },
             'other.txt': {
               source() {
-                return '';
+                return 'This file with MARCO should not be touched';
               }
             }
           }
         };
 
-        const replacers = [
+        replacers = [
           {
-            searchString: `<!--esi-include-webpack-plugin name=${esiItem.name}-->`,
-            replaceString: tag
+            //searchString: `<!--esi-include-webpack-plugin name=${esiItem.name}-->`,
+            searchString: 'MARCO',
+            replaceString: 'POLO'
+          },
+          {
+            searchString: 'CHICKEN',
+            replaceString: 'LITTLE'
           }
         ];
       });
-      it('should replace strings based on replacers', () => {
 
+      it('should replace strings based on replacers', () => {
+        const plugin = new EsiIncludeWebpackPlugin({
+          esi: [{ name: 'scooby', src: 'doo' }]
+        });
+        plugin.replacers = replacers;
+        plugin.manipulateCompilationAssets(compilation);
+        expect(compilation.assets['test.html'].source()).toEqual('This string has a key POLO and has a second one POLO and a third one LITTLE');
       });
+
       it('should only alter file extensions html, htm, ejs', () => {
 
       });
