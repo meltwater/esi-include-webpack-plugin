0790af1696ffc8713cbe72bb42fe8fd11ee355e3
Doug Sappet <dsappet@gmail.com>
2020-03-31 22:10:53 -0400

    fix: Fixing a major bug with async apply function

--- demo/webpack.config.example5.js
+++ demo/webpack.config.example5.js
@@ -1,13 +1,23 @@
 // In this example no html files are operated on even though the plugin is added
+// html-loader and requiring an html file in the JS does not pull the html file into webpack context
 
 const path = require('path');
 const EsiIncludeWebpackPlugin = require('../src/index'); // require('@meltwater/esi-include-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index-require-html.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'html-loader'
+      }
+    ]
   },
   plugins: [
     new EsiIncludeWebpackPlugin({
